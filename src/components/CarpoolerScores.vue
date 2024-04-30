7875d12aac794e792cf2999b611f4ea601ac5a1c
Morgan Courbet <github@sub.ploki.fr>
2024-04-30 17:49:17 +0200

    feat: refresh list of scores upon refresh

--- src/components/CarpoolerScores.vue
+++ src/components/CarpoolerScores.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
-import { type Score } from '@/types'
+import { type Carpooler, type Score } from '@/types'
+import { onMounted, ref } from 'vue'
 
-defineProps({
-  scores: {
-    type: Array<Score>,
-    required: true
+const scores = ref<Score[]>([])
+
+function refreshData() {
+  scores.value = fetchData()
+
+  function fetchData(): Score[] {
+    console.log('Fetching scores…')
+    const alice: Carpooler = { id: 'alice', displayName: 'Alice' }
+    const bob: Carpooler = { id: 'bob', displayName: 'Bob' }
+    const charlie: Carpooler = { id: 'charlie', displayName: 'Charlie' }
+
+    return [
+      { carpooler: alice, score: 0.3 },
+      { carpooler: bob, score: 0.5 },
+      { carpooler: charlie, score: -1 }
+    ]
   }
-})
+}
+
+onMounted(refreshData)
 </script>
 
 <template>
--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,10 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import CarpoolerScores from '@/components/CarpoolerScores.vue'
 import CarpoolerList from '@/components/CarpoolerList.vue'
-import type { Carpooler, Score } from '@/types'
-
-const alice: Carpooler = { id: 'alice', displayName: 'Alice' }
-const bob: Carpooler = { id: 'bob', displayName: 'Bob' }
-const charlie: Carpooler = { id: 'charlie', displayName: 'Charlie' }
-
-const scores: Score[] = [
-  { carpooler: alice, score: 0.3 },
-  { carpooler: bob, score: 0.5 },
-  { carpooler: charlie, score: -1 }
-]
 
 const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/scores' },
-  { path: '/scores', component: CarpoolerScores, props: { scores } },
+  { path: '/scores', component: CarpoolerScores },
   { path: '/carpoolers', component: CarpoolerList }
 ]
 
